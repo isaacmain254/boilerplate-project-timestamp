1a863040547900f115b26e0d9dc7958129b5ee95
isaacmain254 <isaacmain72@gmail.com>
2023-11-24 15:36:22 +0300

    Timestamp microservice complete

--- index.js
+++ index.js
@@ -23,6 +23,27 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
+
+app.get("/api/:date?", function (req, res) {
+  let date = req.params.date;
+  let dateObject;
+  if (!date) {
+    dateObject = new Date();
+  } else if (/\d{5,}/.test(date)) {
+    date = parseInt(date);
+    dateObject = new Date(date);
+  } else {
+    dateObject = new Date(date);
+  }
+  if (dateObject.toString() === "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({
+      unix: dateObject.getTime(),
+      utc: dateObject.toUTCString(),
+    });
+  }
+})
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
